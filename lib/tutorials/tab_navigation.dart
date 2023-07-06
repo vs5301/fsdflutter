de9d0986cc0646f02938f577a09b6d646e21bddf
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-07-06 13:59:48 +0530

    Date & Time

--- lib/tutorials/tab_navigation.dart
+++ lib/tutorials/tab_navigation.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TabBarPage extends StatelessWidget {
+class TabBarPage extends StatefulWidget {
   const TabBarPage({super.key});
 
   @override
+  State<TabBarPage> createState() => _TabBarPageState();
+}
+
+class _TabBarPageState extends State<TabBarPage> {
+  @override
   Widget build(BuildContext context) {
+
+    var time = DateTime.now();
 
     return MaterialApp(
       theme: ThemeData(
@@ -12,74 +19,69 @@ class TabBarPage extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-              title: const Text("My Tabs"),
-              bottom: const TabBar(tabs: [
-                Tab(
-                  icon: Icon(Icons.card_giftcard),
-                  child: Text("Page 1"),
-                ),
-                Tab(
-                  icon: Icon(Icons.person),
-                  child: Text("Page 2"),
-                ),
-              ])),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.black,
-                  child: InkWell(
-                    child: ElevatedButton(
-                      child: Text("Click Here"),
-                      onPressed: (){
-
-                      },
-                    ),
-                    onTap: (){
-                      Text("Button Tapped", style: TextStyle(color: Colors.white),);
-                    },
-                    onLongPress: (){
-                      Text("Button Double Tapped", style: TextStyle(color: Colors.white),);
-                    },
-                    onDoubleTap: (){
-                      Text("Button Long Pressed", style: TextStyle(color: Colors.white),);
-                    },
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+                title: const Text("My Tabs"),
+                bottom: const TabBar(tabs: [
+                  Tab(
+                    icon: Icon(Icons.card_giftcard),
+                    child: Text("Date and Time"),
                   ),
-                )
-              ),
-              Center(
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.black,
-                  child: InkWell(
-                    child: ElevatedButton(
-                      child: Text("Click Here"),
-                      onPressed: (){
-
-                      },
-                    ),
-                    onTap: (){
-                      Text("Button Tapped", style: TextStyle(color: Colors.white),);
-                    },
-                    onLongPress: (){
-                      Text("Button Double Tapped", style: TextStyle(color: Colors.white),);
-                    },
-                    onDoubleTap: (){
-                      Text("Button Long Pressed", style: TextStyle(color: Colors.white),);
-                    },
+                  Tab(
+                    icon: Icon(Icons.person),
+                    child: Text("Page Navigation"),
                   ),
-                ),
-              )
-            ]
-          ),
-        )
+                ])),
+            body: TabBarView(
+                children: [
+                  Center(
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Current Time: ${time.hour}:${time.minute}:${time.second}', style: TextStyle(fontSize: 25),),
+                            Container(height: 20,),
+                            ElevatedButton(onPressed: (){
+                              setState(() {
+
+                              });
+                            }, child: Text("Refresh"))
+                          ],
+                        ),
+                      ),
+                  ),
+                  Center(
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      color: Colors.black,
+                      child: InkWell(
+                        child: ElevatedButton(
+                          child: Text("Click Here"),
+                          onPressed: (){
+                            Navigator.pushNamed(context, "/pageOne");
+                          },
+                        ),
+                        onTap: (){
+                          Text("Button Tapped", style: TextStyle(color: Colors.white),);
+                        },
+                        onLongPress: (){
+                          Text("Button Double Tapped", style: TextStyle(color: Colors.white),);
+                        },
+                        onDoubleTap: (){
+                          Text("Button Long Pressed", style: TextStyle(color: Colors.white),);
+                        },
+                      ),
+                    ),
+                  )
+                ]
+            ),
+          )
       ),
     );
   }
 }
+
